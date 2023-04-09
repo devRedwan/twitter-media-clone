0e6430e1c5352bd2accd7cce85886cbb282a050b
Redwan Ahmed <redwanahmed.dev@gmail.com>
2023-04-09 15:48:29 -0400

    updated to allow typeErrors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["pbs.twimg.com"], // Add any other external image hostnames you want to allow
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
