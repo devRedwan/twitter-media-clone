22528e1b1d8f54e60cf9fceaf268aefd45b1633a
Redwan Ahmed <redwanahmed.dev@gmail.com>
2023-04-09 15:50:09 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,16 +4,4 @@ module.exports = {
   images: {
     domains: ["pbs.twimg.com"], // Add any other external image hostnames you want to allow
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
